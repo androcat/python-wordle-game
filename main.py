85d1dcf84439d606f4077d0036f2f6cc74076faa
Jay Medina <jay.medina.celis@gmail.com>
2023-02-09 14:51:16 -0500

    Chooses random word from list and prints instructions/guide

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+from random import choice
+
 def prGreen(skk): print("\033[92m {}\033[00m".format(skk), end = " ")
- 
 def prYellow(skk): print("\033[93m {}\033[00m".format(skk), end = " ")
 
-word_list = []
-word = "happy"
+word_list = ["happy", "apple", "table", "tiger", "paper", "cider", "dryer", "water", "diner"]
+word = choice(word_list)
 
 counter = 6
+
+print("""WORDLE is game where you guess a 5 letter word.
+Correct letters in the correct spot will appear green
+Correct letters in the wrong spot will appear yellow""")
+
 print("_ _ _ _ _")
 
 while counter > 0:
